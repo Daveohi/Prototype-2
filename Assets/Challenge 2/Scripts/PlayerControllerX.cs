d2a45c06223ff437284ab34e4557b5d27a24d787
David Ohiosimuan Ohimai <daveohimai@gmail.com>
2022-09-03 14:10:20 +0100

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    //public float horizontalInput = 10.0f;
+    //public float speed = 30.0f;
+    
+    public GameObject dogPrefab;
+
+    private float counter = 0;
+    private float dogDelay = 0.3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        //horizontalInput = Input.GetAxis("Horizontal");
+        //transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && (counter > dogDelay))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            counter = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    //public float leftBoundary = -15;
+    //public float rightBoundary = 15;
+    public float xRange = 17;
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        //To move left inbound
+        //if (transform.position.x < -15)
+        //{
+            //transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+        //}
+
+        //To move left or right at a distance
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    }
+}
